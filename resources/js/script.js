af8c99943bdb8b552352a5d3665467cb446fbeca
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-01-23 12:17:08 -0500

    updated jQueary sticky nav Actionable

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,25 @@
-// TEST FUNCTION
-/* 
 $(document).ready(function() {
-  $('h1').click(function() {
-    $(this).css('background-color', '#ff0000');
-  });
+  $('.js--section-features').waypoint(
+    function(direction) {
+      if (direction == 'down') {
+        // If Scrolling Down... Then, Sticky Navigation Should Appear
+        $('nav').addClass('sticky');
+      } else {
+        // If Scrolling Up... Then, Remove Sticky Navigation
+        $('nav').removeClass('sticky');
+      }
+    },
+    {
+      offset: '60px'
+    }
+  );
+
+  //   var waypoints = $('#handler-first').waypoint(
+  //     function(direction) {
+  //       notify(this.element.id + ' hit 25% from top of window');
+  //     },
+  //     {
+  //       offset: '25%'
+  //     }
+  //   );
 });
-*/
